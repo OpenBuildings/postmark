1bc1f3657fe8172d429efcb51195b507168f80ee
Julian Mallett <julian.mallett@gmail.com>
2016-03-30 11:40:56 +1000

    Adding support for Postmark Tags

--- src/Swift_Transport_PostmarkTransport.php
+++ src/Swift_Transport_PostmarkTransport.php
@@ -158,6 +158,11 @@ class Swift_Transport_PostmarkTransport implements \Swift_Transport
             $data['HtmlBody'] = $html->getBody();
         }
 
+        $tag = $message->getId();
+        if (strlen($tag)) {
+            $data['Tag'] = $tag;
+        }
+
         if ($message->getChildren()) {
             $data['Attachments'] = array();
 
